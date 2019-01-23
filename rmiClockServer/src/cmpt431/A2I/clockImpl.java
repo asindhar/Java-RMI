951045ddd0307a07159e2116cc7585e376f6e1cb
Ahsan Naveed <ahsannaveed007@yahoo.com>
2019-01-23 14:42:38 -0800

    date formatting

--- rmiClockServer/src/cmpt431/A2I/clockImpl.java
+++ rmiClockServer/src/cmpt431/A2I/clockImpl.java
@@ -3,7 +3,6 @@ package cmpt431.assignment1;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.Date;
-import java.time.*;
 
 public class clockImpl extends UnicastRemoteObject implements clockInterface {
 
@@ -13,9 +12,8 @@ public class clockImpl extends UnicastRemoteObject implements clockInterface {
     }
 
     // Implementation of the query interface
-    public Instant getServerTime() throws RemoteException {
-        // send a date object as a response
-        //return (new Date());
-        return(java.time.Instant.now());
+    public Date getServerTime() throws RemoteException {
+        // sends a date object as a response
+        return (new Date());
     }
 }
